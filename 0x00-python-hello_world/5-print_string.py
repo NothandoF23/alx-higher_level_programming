3ffaf788ca7c7f49894013b008ab7b37b96120ce
Nothando <fai231kalwandle@gmail.com>
2024-01-15 13:40:28 -0800

    8 tasks done

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+substr = str[:9]
+repeat = str * 3
+print("{} {}".format(repeat, substr))
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,2 @@
+First python project
+Working with python scripts, shell scripts and C scripts
