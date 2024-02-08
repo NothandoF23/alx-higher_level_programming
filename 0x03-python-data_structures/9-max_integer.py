8d2dc1f1964d8718585d5b9da124d02383adc0e0
Nothando <fai231kalwandle@gmail.com>
2024-02-08 11:36:21 -0800

    added max int file

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        size = len(my_list) - 1
+        i = 1
+        maxx = my_list[0]
+        while i < size:
+            if maxx < my_list[i]:
+                maxx = my_list[i]
+            i += 1
+        return (maxx)
+    else:
+        return (None)
