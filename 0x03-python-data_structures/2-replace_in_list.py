d3f230439d38567d71045523bbd438f1efacee9b
Nothando <fai231kalwandle@gmail.com>
2024-02-08 10:39:40 -0800

    updated replace file

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     size = len(my_list) - 1
-    if idx < 0 and idx > size:
+    if idx < 0:
+        return (my_list)
+    if idx > size:
         return (my_list)
     else:
         my_list[idx] = element
