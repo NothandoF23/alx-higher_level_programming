512ea17891a18eb536743631f3efd99aedaa4f33
Nothando <fai231kalwandle@gmail.com>
2024-02-12 12:43:25 -0800

    updated task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,18 +5,18 @@ class Rectangle:
     """Represents a Rectangle class."""
 
     def __init__(self, width=0, height=0):
+
         """Initialise a new Rectangle.
 
         Args:
             width (int): width of a Rectangle
             height (int): height of a Rectangle
             """
-
-            self.width = width
-            self.height = height
+        self.width = width
+        self.height = height
 
     @property
-     def width(self):
+    def width(self):
         """Retrieve the width of the rectangle."""
         return self.__width
 
@@ -32,7 +32,7 @@ class Rectangle:
     def height(self):
         """Retrieve the height of the rectangle."""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
